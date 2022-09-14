2546324ac778eff830096aefbd31a8b888ec0421
Marcon2509 <marcus.duigan@gmail.com>
2022-09-14 17:05:28 +0800

    Set up for A1.3

--- app/server.js
+++ app/server.js
@@ -2,11 +2,6 @@
 
 const express = require('express');
 
-// Constants
-//const PORT = 8080; //8080
-//const HOST = '0.0.0.0';
-
-// App
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World');
